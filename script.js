985d88cc9be8124a3d119d7d3f31d6dfc4712fa3
unknown <shivamshah1210@gmail.com>
2023-08-02 02:23:10 +0530

    almost done

--- script.js
+++ script.js
@@ -40,11 +40,33 @@ document.querySelector("#hello h1").textContent.split("").forEach(function (dets
     document.querySelector("#hello h1").innerHTML = clutter;
 })
 
+var timel = gsap.timeline();
+var i=0;
+var img_slider = setInterval(function(){
+
+  document.querySelector("#loader img").src = `${imgs[i]}`
+  i++;
+  // console.log(imgs[i]);
+  if(i>=imgs.length){
+    clearInterval(img_slider);
+    timel.to("#loader-img img",{
+      y:"-100%",
+      ease:Power3.easeOut,
+    },"shivam")
+    timel.to("#loader",{
+      y:"-100%",
+      delay:-2,
+      ease:Power3.easeOut,
+    },"shivam")
+    
+  }
+
+},200)
 gsap.from("#hello h1 span",{
-  y:"100%",
-  duration:1,
+  y:`100%`,
   stagger:0.1,
-  ease:Expo.easeInOut,
+  duration:0.8,
+  delay:2.6
 })
 
 gsap.to("#page1-video",{
@@ -74,38 +96,64 @@ gsap.to("#h1-section",{
 var cursor = document.querySelector("#cursor")
 
 window.addEventListener("mousemove", function(dets){
-  cursor.style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
+  
+  gsap.to("#cursor",{
+    x:`${dets.clientX - 150}px`,
+    y:`${dets.clientY -150}px`,
+  })
+  dets.stopPropagation();
   // console.log(dets);
 })
-document.querySelectorAll(".project-video").forEach(function(dets){
-  dets.addEventListener("mouseenter",function(){
-    cursor.style.opacity=1
-    dets.style.scale= 0.96
-    dets.lastElementChild.style.scale = 1.08
+// document.querySelectorAll(".project-video").forEach(function(dets){
+//   dets.addEventListener("mouseenter",function(){
+//    
+//     cursor.style.left="-50px"
+//     cursor.style.top="-50px"
+//     dets.style.scale= 0.96
+//     dets.lastElementChild.style.scale = 1.08
+//   })
+// })
+// document.querySelectorAll(".project-video").forEach(function(dets){
+//   dets.addEventListener("mouseleave",function(){
+//     cursor.style.display = "none"
+//     dets.style.scale= 1
+//     dets.lastElementChild.style.scale = 1
+
+
+//   })
+// })
+// document.querySelectorAll(".project-img").forEach(function(dets){
+//   dets.addEventListener("mouseenter",function(){
+//     cursor.style.display = "initial"
+//     dets.style.scale= 0.96
+//     dets.lastElementChild.style.scale = 1.08
+
+//   })
+// })
+// document.querySelectorAll(".project-img").forEach(function(dets){
+//   dets.addEventListener("mouseleave",function(){
+//     
+
+//   })
+// })
+
+var imgs = ['https://wethinkelastic.com/assets/images/11d1acd8abd7b1efc4c23d.svg','https://wethinkelastic.com/assets/images/109804494fa1878703f2c1.svg','https://wethinkelastic.com/assets/images/9e5b57420355774e0a99d.svg','https://wethinkelastic.com/assets/images/8a2db0cd90582eb4b877d.svg','https://wethinkelastic.com/assets/images/7c9e22462b51ae93b5a17.svg','https://wethinkelastic.com/assets/images/6a714ad31db5d83bc967b.svg','https://wethinkelastic.com/assets/images/556ce542d7fdbd78d032e.svg','https://wethinkelastic.com/assets/images/4e7e9b9fc472434d65a23.svg','https://wethinkelastic.com/assets/images/338b04b3ad8edf6773599.svg','https://wethinkelastic.com/assets/images/24a07612b16472c6a503f.svg','https://wethinkelastic.com/assets/images/152c223f4359675788470.svg']
+
+// console.log(imgs.length);
+
+console.log(document.querySelectorAll(".project"));
+
+document.querySelectorAll(".project").forEach(function(dets){
+  dets.addEventListener("mouseenter",(events)=>{
+    
+    console.log(events.bubbles);
+    cursor.style.display = "initial";
+    events.stopPropagation();
   })
-})
-document.querySelectorAll(".project-video").forEach(function(dets){
-  dets.addEventListener("mouseleave",function(){
-    cursor.style.opacity=0
-    dets.style.scale= 1
-    dets.lastElementChild.style.scale = 1
-
-
-  })
-})
-document.querySelectorAll(".project-img").forEach(function(dets){
-  dets.addEventListener("mouseenter",function(){
-    cursor.style.opacity=1
-    dets.style.scale= 0.96
-    dets.lastElementChild.style.scale = 1.08
-
-  })
-})
-document.querySelectorAll(".project-img").forEach(function(dets){
-  dets.addEventListener("mouseleave",function(){
-    cursor.style.opacity=0
-    dets.style.scale= 1
-    dets.lastElementChild.style.scale = 1
-
+  dets.addEventListener("mouseleave",(event)=>{
+    
+    // console.log(event.bubbles);
+    cursor.style.display = "none"
+    event.stopPropagation();
   })
 })
